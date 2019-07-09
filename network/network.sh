7f0bd226d472d6aceef66effe914f553e1dc8b5a
fabric <futureds@naver.com>
2019-07-09 16:23:46 +0900

    기능개선
    stop.sh 실행시 체인코드를 갖는 모든 컨테이너와 이미지가 삭제되도록 변경.

--- network/network.sh
+++ network/network.sh
@@ -88,7 +88,7 @@ networkStop() {
 
 
 function clearContainers() {
-  CONTAINER_IDS=$(docker ps -a | awk '($2 ~ /dev-peer.*.setcc.*/) {print $1}')
+  CONTAINER_IDS=$(docker ps -a | awk '($2 ~ /dev-peer*/) {print $1}')
   if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" == " " ]; then
     echo "---- No containers available for deletion ----"
   else
@@ -97,7 +97,7 @@ function clearContainers() {
 }
 
 function removeUnwantedImages() {
-  DOCKER_IMAGE_IDS=$(docker images | awk '($1 ~ /dev-peer.*.setcc.*/) {print $3}')
+  DOCKER_IMAGE_IDS=$(docker images | awk '($1 ~ /dev-peer*/) {print $3}')
   if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" == " " ]; then
     echo "---- No images available for deletion ----"
   else
